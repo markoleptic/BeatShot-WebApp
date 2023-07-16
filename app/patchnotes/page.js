93dbd9e55ea039d40d4b57ff24a6c2a69bca3c23
markoleptic <markalexcham@gmail.com>
2023-07-16 13:33:43 -0500

    Fix Patch Notes

--- app/patchnotes/page.js
+++ app/patchnotes/page.js
@@ -3,19 +3,15 @@ import React from "react";
 const PatchNotes = () => {
   return (
     <>
-      <div className="flex-container-column">
-        <div className="content">
-          <div className="content-header">
+      <div className="flex-container-column gap-1rem padding-1rem">
             <h2 className="pn-title">Patch Notes</h2>
-          </div>
-          <div className="content-main">
+          <div className="responsive-centered-container">
             <div className="pn-wrapper">
               <p className="fs-400 text-center text-lightgrey">
                 Patch Notes will become available after BeatShot has entered
                 Early Access. In the meantime you can check out the <Link className="link text-white hover-blue" href="https://github.com/markoleptic/BeatShot">Github</Link> for the project.
               </p>
             </div>
-          </div>
         </div>
       </div>
     </>
